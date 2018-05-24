bf78e63f7364a677e7adf28e711f3937cd94546c
DutchGhost <kasper199914@gmail.com>
2018-05-24 21:10:38 +0200

    added crate level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(feature="nightly", feature(test))]
 
+//! This crates offers functions to convert from (unsigned for now) integers to bytes, and from bytes to integers.
+//! The goal of this crate is to do it's operations fast. This also means that weird corner cases, are not really handled.
+//! If such a corner case can occur, this is noted in the # Safety section, and shows the corner case.
+
 pub mod convert;
 
 #[cfg(feature="nightly")]
