5f50199e293e7b5c34a6397e0ff618ad548e03a7
DutchGhost <kasper199914@gmail.com>
2018-09-22 22:25:09 +0200

    int_to_bytes works for unsigned

--- src/into_ascii.rs
+++ src/into_ascii.rs
@@ -43,21 +43,24 @@ macro_rules! impl_unsigned_conversions {
             }
         
             fn int_to_bytes(mut self, buff: &mut [u8]) {
+                // [1, 2, 3, 4, 5].exact_chunks(2).rev() gives [3, 4] and [2, 1],
+                // while we wanted [4, 5] and [2, 3].
+                // So we make the remainder ourselves.
                 let rem = buff.len() % 4;
                 let splitof = buff.len() - (buff.len() - rem);
-
-                let (mut remainder, mut v) = buff.split_at_mut(splitof);
-
+                let (remainder, v) = buff.split_at_mut(splitof);
+        
                 for mut chunk in v.exact_chunks_mut(4).rev() {
                     let q = self / 10;
                     let q1 = self / 100;
                     let q2 = self / 1000;
         
                     let r = (self % 10) as u8 + ASCII_TO_INT_FACTOR;
-                    let r1 = (q % 10) as   u8 + ASCII_TO_INT_FACTOR;
-                    let r2 = (q1 % 10) as  u8 + ASCII_TO_INT_FACTOR;
-                    let r3 = (q2 % 10) as  u8 + ASCII_TO_INT_FACTOR;
-
+                    let r1 = (q % 10) as u8 + ASCII_TO_INT_FACTOR;
+                    let r2 = (q1 % 10) as u8 + ASCII_TO_INT_FACTOR;
+                    let r3 = (q2 % 10) as u8 + ASCII_TO_INT_FACTOR;
+        
+                    // @NOTE: Make me nicer when NLL hits stable
                     match &mut chunk {
                         &mut [ref mut b3, ref mut b2, ref mut b1, ref mut b] => {
                             *b = r;
@@ -65,12 +68,12 @@ macro_rules! impl_unsigned_conversions {
                             *b2 = r2;
                             *b3 = r3;
                         }
-                        _ => unreachable!()
+                        _ => unreachable!(),
                     }
-
+        
                     self /= 10_000;
                 }
-
+        
                 for byte in remainder.iter_mut().rev() {
                     let q = self / 10;
                     let r = (self % 10) as u8 + ASCII_TO_INT_FACTOR;
@@ -78,7 +81,7 @@ macro_rules! impl_unsigned_conversions {
         
                     //there's nothing more to do.
                     if q == 0 {
-                        return;
+                        break;
                     }
         
                     self = q;
@@ -112,3 +115,13 @@ impl_unsigned_conversions!(u16);
 impl_unsigned_conversions!(u32);
 impl_unsigned_conversions!(u64);
 impl_unsigned_conversions!(usize);
+
+#[cfg(test)]
+mod tests {
+    use super::IntoAscii;
+
+    #[test]
+    fn int_to_bytes_usize() {
+        assert_eq!(99_999_9999usize.itoa(), vec![b'9'; 9]);
+    }
+}
